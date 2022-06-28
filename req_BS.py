219df8613d68bd7e120001e2f1614b3c37c065d0
Alan Zhang <zhan4096@purdue.edu>
2022-06-28 15:32:43 -0500

    Beginning Scraping

---
+++ req_BS.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+import sys 
+
+stdoutOrigin=sys.stdout 
+sys.stdout = open("log.txt", "w")
+
+
+url = "http://books.toscrape.com/"
+req = requests.get(url)
+content = req.text
+
+soup=BeautifulSoup(content, 'html.parser')
+print(soup.prettify())
+
+
+
+def ifnull(var, val):
+  if var is None:
+    return val
+  return var
+sys.stdout.close()
+sys.stdout=stdoutOrigin
+
+'''brand_name = []
+price = []
+location = []
+description = []
+rating_score = []
+raw = soup.findAll('script')[3].text
+page = pd.read_json(raw.split("window.pageData=")[1],orient='records')
+for item in page.loc['listItems','mods']:
+    brand_name.append(item['brandName'])
+    price.append(item['price'])
+    location.append(item['location'])
+    description.append(ifnull(item['description'],0))
+    rating_score.append(ifnull(item['ratingScore'],0))
+output=pd.DataFrame({'brandName':brand_name,'price':price,'location':location,'description':description,'rating score':rating_score})
+output.to_csv('output.csv',index=False)'''
